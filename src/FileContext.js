ccd1e5336b967cb8434e706bffbc1898c3b0385b
americanotwoshot <127121005+americanotwoshot@users.noreply.github.com>
2024-02-20 12:31:10 +0900

    Update FileContext.js

--- src/FileContext.js
+++ src/FileContext.js
@@ -4,6 +4,7 @@ export const FileContext = createContext();
 
 export const FileProvider = ({ children }) => {
   const [files, setFiles] = useState([]);
+  const [filesContent, setFilesContent] = useState([]);
 
   return (
     <FileContext.Provider value={{ files, setFiles }}>
